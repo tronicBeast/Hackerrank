0470aa335ed20fca70a4bacceafb8348179d4a4f
Rahat Kumar <rahatkumar1997@icloud.com>
2017-06-04 15:43:45 +0530

    Aaah.cpp

---
+++ Aaah!.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    
+    if(s1.length()>=s2.length()){
+        cout<<"go";
+    }
+    else
+        cout<<"no";
+return 0;
+}
